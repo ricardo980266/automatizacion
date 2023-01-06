5d934801396eb3f7a3cc870af1fe7df002520003
Ricardo Mario Caro Tapias <rmcaro@personalsoft.com.co>
2023-01-06 15:43:37 -0500

    Nuevo Error

--- src/test/java/runners/runnersTest.java
+++ src/test/java/runners/runnersTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.DataProvider;
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
-        tags = "@regression", monochrome = true, dryRun = false,
+        tags = "@login", monochrome = true, dryRun = false,
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 
 
