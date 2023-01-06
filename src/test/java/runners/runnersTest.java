3c5f339ec8a30a25e7811cb043d29edc9bba5e07
Ricardo Mario Caro Tapias <rmcaro@personalsoft.com.co>
2023-01-06 11:33:03 -0500

    Solucion de errores/EjecucionParalela

--- src/test/java/runners/runnersTest.java
+++ src/test/java/runners/runnersTest.java
@@ -1,14 +1,20 @@
 package runners;
 
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+//import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
-        tags = "@contact-us", monochrome = true, dryRun = false,
-        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
-public class runners extends AbstractTestNGCucumberTests {
 
+//@RunWith(Cucumber.class)
+@CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
+        tags = "@regression", monochrome = true, dryRun = false,
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+
+
+public class runnersTest extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
--- target/test-classes/features/Login.feature
+++ target/test-classes/features/Login.feature
@@ -1,4 +1,4 @@
-@Login
+@Login @regression
 Feature: WebDriver university - Login Page
 
   Background:
