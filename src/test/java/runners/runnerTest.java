7958d51404544074f03f206d1aeeedab7f492e28
jcesarmunoz <jcesarmerchan@gmail.com>
2023-01-11 12:03:24 -0500

    arreglo pom

--- src/test/java/runners/runnerTest.java
+++ src/test/java/runners/runnerTest.java
@@ -1,14 +1,15 @@
 package runners;
 
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
+
+
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
-        tags = "@regression", monochrome = true, dryRun = false,
+        tags = "", monochrome = true, dryRun = false,
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 
 
@@ -19,6 +20,5 @@ public class runnerTest extends AbstractTestNGCucumberTests {
         return super.scenarios();
     }
 }
-//public class runnerTest {
-//}
+
 
